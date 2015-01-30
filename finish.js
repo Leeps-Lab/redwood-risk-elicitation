bc5b5efeb13e987337697a8762750b7d94fb7e2c
Matthew Jee <matthew@marmphco.com>
2015-01-30 00:17:17 -0800

    fixes bug where payout page would fail to load completely when some views were not used

--- finish.js
+++ finish.js
@@ -1,3 +1,18 @@
 Redwood.controller("REFinishController", ["$scope", "RedwoodSubject", function($scope, rs) {
-    // does anything actually need to be here?
+    $scope.shouldShow = {
+        "RevealedPreferences": false,
+        "PortfolioAllocation": false,
+        "HoltLaury": false
+    }
+
+    rs.on_load(function() {
+        // Determine table visibility based on configuration
+        // If an experiment view appears in the configuration,
+        // then the corresponding payout table should be shown
+        for (key in $scope.shouldShow) {
+            $scope.shouldShow[key] = rs.configs.filter(function(config) {
+                return config.view === key;
+            }).length > 0;
+        }
+    })
 }]);
